a49de7d0128fadd062f6c80a7b8083a491088544
lfolco <me@laurafolco.com>
2017-04-01 13:46:58 -0700

    fix registration.php

--- src/registration.php
+++ src/registration.php
@@ -1,5 +1,7 @@
 <?php
 
-use \Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Stroopwafel_Sysconfsearch', __DIR__);
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Stroopwafel_Sysconfsearch',
+    __DIR__
+);
