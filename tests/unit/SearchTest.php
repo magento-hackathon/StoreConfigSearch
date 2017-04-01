2e3efbc95480325d1bc360e54eaf2e1108d0da62
Sander Mangel <sander@sandermangel.nl>
2017-04-01 14:18:57 -0700

    added testing framework

---
+++ tests/unit/SearchTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Stroopwafel\Searchsysconf\Tests;
+
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+
+/**
+ * @covers
+ */
+class SearchTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSearchPhrase()
+    {
+        echo "hello world";
+    }
+}
